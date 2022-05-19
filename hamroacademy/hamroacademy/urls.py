0b91bd77b2d3353dfba7b79cd776eea3a0ea1d86
maheshwor003 <maheshworshakha33@gmail.com>
2022-05-19 11:03:16 +0545

    CRUD from Jquery and Ajax

--- hamroacademy/hamroacademy/urls.py
+++ hamroacademy/hamroacademy/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from student import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('save/',views.save_data,name='save'),
     path('delete/',views.delete_data,name = "delete"),
     path('edit/',views.edit_data,name = "edit"),
+    path('list/',views.list_data,name = "listdata"),
+    path('login/',views.login, name='login' ),
 ]
